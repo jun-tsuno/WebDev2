ba86c6ee44ce7df5fc6370f2b46b3a903c4c3b90
Jun <jun55tsuno@gmail.com>
2023-02-07 11:56:54 -0800

    updated

--- midterm_project/main.js
+++ midterm_project/main.js
@@ -4,15 +4,26 @@ const cardTemplate = $("#card-temp").html();
 const cartTemplate = $("#cart-temp").html();
 const addAlert = $("#add-alert").html();
 
-const cartItems = [];
+let cartItems = [];
+
+const handleDelete = (e) => {
+	$(e.target.parentElement).remove();
+
+	const newCartItems = cartItems.filter((item) => {
+		return item.id !== Number(e.target.parentElement.getAttribute("data-id"));
+	});
+	cartItems = newCartItems;
+	counter();
+};
 
 const handleAdd = async (e) => {
 	const clickedParent = e.target.parentElement;
-	console.log(clickedParent);
 	const clonedCartTemplate = $(cartTemplate).clone(true);
+
 	const addTitle = $(clickedParent).find(".card-title").html();
 	const addPrice = $(clickedParent).find(".card-price").html();
-	const data = { title: addTitle, price: parseInt(addPrice) };
+	const dataId = Math.random();
+	const data = { id: dataId, title: addTitle, price: parseInt(addPrice) };
 
 	await axios
 		.post("https://run.mocky.io/v3/4d89e4c9-4108-41c8-814e-554d5ac7bc7e", data)
@@ -21,13 +32,14 @@ const handleAdd = async (e) => {
 			cartItems.push(addedItem);
 
 			$(clonedCartTemplate).find(".add-title").html(addedItem.title);
-			$(clonedCartTemplate).find(".add-price").html(addedItem.price);
+			$(clonedCartTemplate).find(".add-price").html(`$ ${addedItem.price}`);
+			$(clonedCartTemplate).attr("data-id", dataId);
 			$(cartList).after(clonedCartTemplate);
-			countAdd();
+			counter();
 		});
 };
 
-const countAdd = () => {
+const counter = () => {
 	$("#cart-badge").html(cartItems.length);
 	$("#item-count").html(cartItems.length);
 	$("#item-total").html(
@@ -38,6 +50,7 @@ const countAdd = () => {
 };
 
 $(document).ready(async () => {
+	$(itemsList).append($("#spinner-temp").html());
 	await axios
 		.get("https://fakestoreapi.com/products?limit=15")
 		.then(({ data }) => {
@@ -47,12 +60,14 @@ $(document).ready(async () => {
 					$(clonedCard).find(".card-img-top").attr("src", ele.image);
 					$(clonedCard).find(".card-title").text(ele.title);
 					$(clonedCard).find(".card-price").text(ele.price);
+					$("#spinner").remove();
 					$(clonedCard).appendTo(itemsList);
 				});
 			}
 			$(".add-button").on("click", handleAdd);
 		})
 		.catch((error) => console.log(error));
-
-	countAdd();
+	counter();
 });
+
+$(".delete-button").on("click", handleDelete);
