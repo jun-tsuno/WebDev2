5b12bda0eb1e0074a9ab2d24d005abfee4b08043
Jun <jun55tsuno@gmail.com>
2023-02-06 23:10:54 -0800

    updated

--- midterm_project/main.js
+++ midterm_project/main.js
@@ -2,30 +2,39 @@ const itemsList = $("#items-list");
 const cartList = $("#cart-list");
 const cardTemplate = $("#card-temp").html();
 const cartTemplate = $("#cart-temp").html();
+const addAlert = $("#add-alert").html();
 
 const cartItems = [];
 
-const handleAdd = (e) => {
+const handleAdd = async (e) => {
 	const clickedParent = e.target.parentElement;
-	const addTitle = $(clickedParent).find(".card-title").text();
-	const addPrice = $(clickedParent).find(".card-price").text();
+	console.log(clickedParent);
 	const clonedCartTemplate = $(cartTemplate).clone(true);
-	const data = { title: addTitle, price: addPrice };
+	const addTitle = $(clickedParent).find(".card-title").html();
+	const addPrice = $(clickedParent).find(".card-price").html();
+	const data = { title: addTitle, price: parseInt(addPrice) };
 
-	axios({
-		method: "POST",
-		url: "https://run.mocky.io/v3/4d89e4c9-4108-41c8-814e-554d5ac7bc7e",
-		data,
-	}).then((response) => {
-		const addedItem = JSON.parse(response.config.data);
-		cartItems.push(addedItem);
+	await axios
+		.post("https://run.mocky.io/v3/4d89e4c9-4108-41c8-814e-554d5ac7bc7e", data)
+		.then((res) => {
+			const addedItem = JSON.parse(res.config.data);
+			cartItems.push(addedItem);
 
-		$(clonedCartTemplate).find(".add-title").text(addedItem.title);
-		$(clonedCartTemplate).find(".add-price").text(addedItem.price);
+			$(clonedCartTemplate).find(".add-title").html(addedItem.title);
+			$(clonedCartTemplate).find(".add-price").html(addedItem.price);
+			$(cartList).after(clonedCartTemplate);
+			countAdd();
+		});
+};
 
-		console.log(clonedCartTemplate);
-		$(cartList).after(clonedCartTemplate);
-	});
+const countAdd = () => {
+	$("#cart-badge").html(cartItems.length);
+	$("#item-count").html(cartItems.length);
+	$("#item-total").html(
+		cartItems.reduce((pre, curr) => {
+			return pre + curr.price;
+		}, 0)
+	);
 };
 
 $(document).ready(async () => {
@@ -36,12 +45,14 @@ $(document).ready(async () => {
 				data.map((ele) => {
 					const clonedCard = $(cardTemplate).clone(true);
 					$(clonedCard).find(".card-img-top").attr("src", ele.image);
-					$(clonedCard).find(".card-title").html(ele.title);
-					$(clonedCard).find(".card-price").html(`$ ${ele.price}`);
+					$(clonedCard).find(".card-title").text(ele.title);
+					$(clonedCard).find(".card-price").text(ele.price);
 					$(clonedCard).appendTo(itemsList);
 				});
 			}
 			$(".add-button").on("click", handleAdd);
 		})
 		.catch((error) => console.log(error));
+
+	countAdd();
 });
